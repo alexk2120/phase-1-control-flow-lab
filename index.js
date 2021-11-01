51ce51eb78b32774567bf4f1aab5af518e663839
alexander.Khatamov <alexk2120@gmail.com>
2021-11-01 18:39:28 -0400

    commit tree d63def78e5722ab1f2a3e4a71baec0712a8fc3c8

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500){
+    return "No can do." 
+  }
+   else if(feet > 2000) {
+   return "I will gladly take your thirty bucks."}
+  else if(feet <= 400){
+    return"This one is on me!"
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) { 
+const result = city === "NYC" ? "Ok, sounds good.": "No go.";
+
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+function switchOnCharmFromTip(tip){
+
+switch (tip) {
+  case "generous":{
+       return "Thank you so much."}
+        break;
+  case "not as generous":{          
+        return "Thank you."}
+         break;
+  default: 
+  return "Bye."
+      }
+    }
